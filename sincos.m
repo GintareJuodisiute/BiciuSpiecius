a83a86bcdae2e7026903cfd2e1ed57796ec5f215
Gintare Juodisiute <gjuodisiute@gmail.com>
2013-03-21 11:59:29 +0200

    sukurem scripta sincos grafikui

--- sincos.m
+++ sincos.m
@@ -2,5 +2,9 @@ function fsincos =sincos(x);
 % Garzina funkcijos: f=sin(x)+cos(x) reiksme
 % Paliedimas: f=sincos(x), kur x vienmatis vektorius
 % Pvz. f=sincos(1.0);
+% Leistinoji sritis (poaibis apibrezimo srities): [-10,10];
 fsincos=sin(x)+ cos(x);
+x=[-10:0.2:10];
+fsincos=sin(x)+ cos(x);
+plot(x,fsincos)
 end
