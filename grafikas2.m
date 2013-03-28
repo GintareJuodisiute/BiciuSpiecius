ba553b5ffac1328b3ea7e81d562df00bbec5c8bf
Gintare Juodisiute <gjuodisiute@gmail.com>
2013-03-28 12:51:07 +0200

    Baigtas grafikas2.

--- grafikas2.m
+++ grafikas2.m
@@ -1,5 +1,5 @@
 x1=-10:0.2:10;% padaryti, kad breztu dvimate funkcija: surf, mesh komandos
 x2=-10:0.2:10;
 [X1,X2]=meshgrid(x1,x2);% plot jau nebetiks, reikia naudoti surf, mesh komandos
-Z=sincos2(x);
+Z=sin(X1)+ cos(X2);
 surf(X1,X2,Z)
