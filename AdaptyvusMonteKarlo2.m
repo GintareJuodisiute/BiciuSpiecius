767f3980da0071d7e69fe36e8fb8897d960df591
Gintare Juodisiute <gjuodisiute@gmail.com>
2013-04-17 23:53:41 +0300

    Pataisytas AdaptyvusMonteKarlo2.m

--- AdaptyvusMonteKarlo2.m
+++ AdaptyvusMonteKarlo2.m
@@ -11,8 +11,8 @@ f1=[];
 for i=1:k1 % kad butu tiksliau galima ivesti daugiau tasku
   f1(i)=sincos2(x1(i,:));
 end
-[fMin1,indMin1]=min(f1);% Randamas maximumas ir jo indeksas
-%[fMax1,indMax1]=max(f1);% Randamas minimumas ir jo indeksas
+[fMin1,indMin1]=min(f1);% Randamas minimumas ir jo indeksas
+%[fMax1,indMax1]=max(f1);% Randamas maximumas ir jo indeksas
 xMin1=x1(indMin1,:); %pritaikyti dvimaciui atvejui
 %xMax1=x1(indMax1,:); %pritaikyti dvimaciui atvejui
 fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin1,xMin1(1),xMin1(2))
@@ -23,11 +23,23 @@ scatter(xMin1(1),xMin1(2),'r*');
 text(xMin1(1) + 0.3,xMin1(2),num2str(fMin1));
 rectangle('Position',[-10.0,-10.0,20.0,20.0],...
     'LineWidth',5,'LineStyle','--')
-
-
-
-% kitus 50 bandymu atliekame kitoje sumazintoje srityje  
-a2=xMin1(1)-1; %Likusiu tasku (vektoriu) skaicius
+ 
+ 
+% kitus 50 bandymu atliekame kitoje sumazintoje srityje 
+ 
+if xMin1(1) < -10 + 1 %Cia pridedama arba atimama puse duoto kvadrato krastines
+    xMin1(1) = -10 + 1
+elseif xMin1(1) > 10 - 1
+    xMin1(1) = 10 - 1
+end
+ 
+if xMin1(2) < -10 + 1
+    xMin1(2) = -10 + 1
+elseif xMin1(2) > 10 - 1
+    xMin1(2) = 10 - 1
+end
+ 
+a2=xMin1(1)-1; % 1-tai yra puse duotos krastines, o musu kvadratas 2 ant 2 %Likusiu tasku (vektoriu) skaicius
 b2=xMin1(1)+1;% sumazinta sritis
 a3=xMin1(2)-1;
 b3=xMin1(2)+1;
@@ -35,6 +47,52 @@ n=1;% dimensija(matavimas)
 k2=50; % tasku (vektoriu) skaicius
 x2(:,1)=a2 + (b2-a2).*rand(k2,n);% Sugeneruoja dvimacius vektorius
 x2(:,2)=a3 + (b3-a3).*rand(k2,n);
+f2=[];
+for i=1:k2 % kad butu tiksliau galima ivesti daugiau tasku
+  f2(i)=sincos2(x2(i,:));
+end
+[fMin2,indMin2]=min(f2);% Randamas minimumas ir jo indeksas
+%[fMax2,indMax2]=max(f2);% Randamas maximumas ir jo indeksas
+xMin2=x2(indMin2,:); %pritaikyti dvimaciui atvejui
+%xMax2=x2(indMax2,:); %pritaikyti dvimaciui atvejui
+fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin2,xMin2(1),xMin2(2))
+%fprintf('Surastas max = %6.4f taske x = (%6.4f, %6.4f)\n ', fMax2,xMax2(1),xMax2(2))
+hold on;
+scatter(x2(:,1),x2(:,2),'b.');
+scatter(xMin2(1),xMin2(2),'r+');
+text(xMin2(1) + 0.3,xMin2(2),num2str(fMin2));
+rectangle('Position',[a2,a3,2.0,2.0],... % Cia 2.0, 2.0 tai yra nurodomas kvardrato dydis
+    'LineWidth',3,'LineStyle','--')
+%grafikas2;
+ 
+%{
+%Kitaip gali buti pateiktas,kai fiksuojama puse kvardrato krastines
+ 
+puseKrastines = 0.5;
+ 
+% kitus 50 bandymu atliekame kitoje sumazintoje srityje  
+if xMin1(1) < -10 + puseKrastines
+    xMin1(1) = -10 + puseKrastines
+elseif xMin1(1) > 10 - puseKrastines
+    xMin1(1) = 10 - puseKrastines
+end
+ 
+if xMin1(2) < -10 + puseKrastines
+    xMin1(2) = -10 + puseKrastines
+elseif xMin1(2) > 10 - puseKrastines
+    xMin1(2) = 10 - puseKrastines
+end
+ 
+a2=xMin1(1)-puseKrastines; %Likusiu tasku (vektoriu) skaicius
+b2=xMin1(1)+puseKrastines;% sumazinta sritis
+a3=xMin1(2)-puseKrastines;
+b3=xMin1(2)+puseKrastines;
+n=1;% dimensija(matavimas)
+k2=50; % tasku (vektoriu) skaicius
+ 
+x2(:,1)=a2 + (b2-a2).*rand(k2,n);% Sugeneruoja dvimacius vektorius
+x2(:,2)=a3 + (b3-a3).*rand(k2,n);
+ 
 f2=[];
 for i=1:k2 % kad butu tiksliau galima ivesti daugiau tasku
   f2(i)=sincos2(x2(i,:));
@@ -49,6 +107,7 @@ hold on;
 scatter(x2(:,1),x2(:,2),'b.');
 scatter(xMin2(1),xMin2(2),'r+');
 text(xMin2(1) + 0.3,xMin2(2),num2str(fMin2));
-rectangle('Position',[a2,a3,2.0,2.0],...
+rectangle('Position',[a2,a3,puseKrastines*2,puseKrastines*2],...
     'LineWidth',3,'LineStyle','--')
 %grafikas2;
+%}
