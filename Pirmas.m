6eceed3c4dae3b10fe76bf09523c0bfaa26a9130
Gintare Juodisiute <gjuodisiute@gmail.com>
2013-04-18 14:34:19 +0300

    Patobulintas AdaptyvusMonteKarlo2.m

--- Pirmas.m
+++ Pirmas.m
@@ -1,4 +1,5 @@
-function [fMin2visi,vidurkis] = AdaptyvusMonteKarlo2(funkcija,a1,b1) 
+function [fMin2visi,vidurkis] = Pirmas(funkcija,a1,b1) 
+
 % Monte Carlo (Random Search) motodo realizacija
 % 1. Sugeneruojame 100 atsitiktiniu tasku intervale [-10:10]^2
 % 2. Surandame, kuriame funkcija igyje didziausia (maziausia) reiksme
@@ -7,12 +8,12 @@ function [fMin2visi,vidurkis] = AdaptyvusMonteKarlo2(funkcija,a1,b1)
 %b1=10; %pradine sritis
 %PALEIDIMAS: PVZ. 
 % a1=-10; b1=10; funkcija = @sincos2;
-%[fMin2visi, VID] = AdaptyvusMonteKarlo2(funkcija,a1,b1)
+%[fMin2visi, VID] = Pirmas(funkcija,a1,b1)
 fMin2visi=[];
-for k=1:100
-
+for k=1:10
+    
 n=2;% dimensija(matavimas)
-k1=80; % tasku (vektoriu) skaicius
+k1=70; % tasku (vektoriu) skaicius
 x1=a1 + (b1-a1).*rand(k1,n);% Sugeneruoja dvimacius vektorius
 f1=[];
 for i=1:k1 % kad butu tiksliau galima ivesti daugiau tasku
@@ -22,7 +23,7 @@ end
 %[fMax1,indMax1]=max(f1);% Randamas maximumas ir jo indeksas
 xMin1=x1(indMin1,:); %pritaikyti dvimaciui atvejui
 %xMax1=x1(indMax1,:); %pritaikyti dvimaciui atvejui
-%fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin1,xMin1(1),xMin1(2))
+fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin1,xMin1(1),xMin1(2))
 %fprintf('Surastas max = %6.4f taske x = (%6.4f, %6.4f)\n ', fMax1,xMax1(1),xMax1(2))
 hold on;
 scatter(x1(:,1),x1(:,2),'b.');
@@ -51,7 +52,7 @@ b2=xMin1(1)+1;% sumazinta sritis
 a3=xMin1(2)-1;
 b3=xMin1(2)+1;
 n=1;% dimensija(matavimas)
-k2=20; % tasku (vektoriu) skaicius
+k2=30; % tasku (vektoriu) skaicius
 x2(:,1)=a2 + (b2-a2).*rand(k2,n);% Sugeneruoja dvimacius vektorius
 x2(:,2)=a3 + (b3-a3).*rand(k2,n);
 f2=[];
@@ -62,7 +63,7 @@ end
 %[fMax2,indMax2]=max(f2);% Randamas maximumas ir jo indeksas
 xMin2=x2(indMin2,:); %pritaikyti dvimaciui atvejui
 %xMax2=x2(indMax2,:); %pritaikyti dvimaciui atvejui
-%fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin2,xMin2(1),xMin2(2))
+fprintf('Surastas min = %6.4f taske x = (%6.4f, %6.4f)\n ', fMin2,xMin2(1),xMin2(2))
 %fprintf('Surastas max = %6.4f taske x = (%6.4f, %6.4f)\n ', fMax2,xMax2(1),xMax2(2))
 hold on;
 scatter(x2(:,1),x2(:,2),'b.');
@@ -70,12 +71,12 @@ scatter(xMin2(1),xMin2(2),'r+');
 text(xMin2(1) + 0.3,xMin2(2),num2str(fMin2));
 rectangle('Position',[a2,a3,2.0,2.0],... % Cia 2.0, 2.0 tai yra nurodomas kvardrato dydis
     'LineWidth',3,'LineStyle','--')
- 
+
 fMin2visi=[fMin2visi;fMin2];
 %figure;
 end
 vidurkis = mean(fMin2visi);
-
+ 
 %{
 %Kitaip gali buti pateiktas,kai fiksuojama puse kvardrato krastines
  
